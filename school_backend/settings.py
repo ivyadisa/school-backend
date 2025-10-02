9d48103bac0ed638147d711882fe5ab69053b7e4
ivyadisa <ivyadisa7@gmail.com>
2025-10-02 16:30:14 +0300

    Prepare for Render deployment

--- school_backend/settings.py
+++ school_backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["school-backend-2-oiva.onrender.com"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 
